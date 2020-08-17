96bb3eb4fe71da82a86baec4eac2dad1bdbe3765
horiuchi67 <1234mhoriuchi@gmail.com>
2020-08-17 19:23:34 +0900

    日本語化に対応しました。

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -9,11 +9,12 @@ class BooksController < ApplicationController
   	  @book = Book.new(book_params)
   	  @book.user_id = current_user.id
   	  if @book.save
-      flash[:notice] = "You have creatad book successfully."
+      flash[:notice] = "新規投稿しました。"
   	  redirect_to book_path(@book.id)
     else
-      @books = Book.all
-      render 'index'
+      @book = Book.new
+      flash[:notice] = "新規投稿に失敗しました。"
+      render 'new'
       end
     end
     def index
@@ -40,7 +41,7 @@ class BooksController < ApplicationController
       @book = Book.find(params[:id])
 
       if @book.update(book_params)
-        flash[:notice] = "Book was successfully created."
+        flash[:notice] = "編集しました。"
         redirect_to book_path(@book.id)
       else
         render 'edit'
--- app/models/book.rb
+++ app/models/book.rb
@@ -6,6 +6,7 @@ class Book < ApplicationRecord
 		favorites.where(user_id: user.id).exists?
 	end
   	validates :title, presence:true, length: {minimum: 2,maximum: 20}
+    validates :author, presence:true, length: {minimum: 1,maximum: 20}
 	validates :body, presence:true, length: {maximum: 200}
 	def self.search(method,word)
                 if method == "forward_match"
