8cede9c371b145a23f3dc32e58ab425ca2e0a718
horiuchi67 <1234mhoriuchi@gmail.com>
2020-08-17 20:50:59 +0900

    book_commentsコントローラーを修正しました。

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -4,7 +4,14 @@ class BookCommentsController < ApplicationController
 	    @book = Book.find(params[:book_id])
 	    book_comment = current_user.book_comments.build(book_comment_params)
 	    book_comment.book_id = @book.id
-	    book_comment.save
+	    if book_comment.save
+	    else
+	    	@book = Book.find(params[:book_id])
+	    	@user = @book.user
+	    	@book_comment = BookComment.new
+	    	flash[:notice] = "コメントを入力してください。"
+	    	render 'books/show'
+	    end
 	end
 	def destroy
 		@book = Book.find(params[:book_id])
